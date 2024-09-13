179c7c864d5f62cb86da057190fbd89e30b3005c
ra <118706048+rauliaa@users.noreply.github.com>
2024-09-13 14:41:23 +0700

    Update 2024_09_12_104426_create_recipes_table.php

--- database/migrations/2024_09_12_104426_create_recipes_table.php
+++ database/migrations/2024_09_12_104426_create_recipes_table.php
@@ -11,9 +11,22 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('recipes', function (Blueprint $table) {
+       Schema::create('recipes', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->text('instructions');
+            $table->string('image')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('user_id')->nullable(); // ID pengguna yang menambahkan resep
+            $table->integer('prep_time')->nullable();
+            $table->integer('cook_time')->nullable();
+            $table->integer('servings')->nullable();
             $table->timestamps();
+        
+            // Foreign key ke tabel kategori dan users
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
         });
     }
 
