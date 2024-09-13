882cea22a7ea3246e616f89b796fd1c0bd058fce
theresia2 <118708476+tasyaaa037@users.noreply.github.com>
2024-09-13 13:29:40 +0700

    Migrate Comment & Favorite

--- database/migrations/2024_09_12_104710_create_comments_table.php
+++ database/migrations/2024_09_12_104710_create_comments_table.php
@@ -9,18 +9,22 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('recipe_id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('content');
+            $table->integer('rating')->nullable();
             $table->timestamps();
+
+            $table->foreign('recipe_id')->references('id')->on('recipes')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('comments');
     }
--- database/migrations/2024_09_12_104720_create_favorites_table.php
+++ database/migrations/2024_09_12_104720_create_favorites_table.php
@@ -9,18 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('favorites', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('recipe_id');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('recipe_id')->references('id')->on('recipes')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('favorites');
     }
